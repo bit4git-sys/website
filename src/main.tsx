75d797cb50cb0e3afd0ff15e910e1628f28dd7fb
uduthashashidhar <shashi400066@gmail.com>
2025-12-06 12:08:59 +0530

    chore: rollback logo, favicon, and background to previous setup

--- src/main.tsx
+++ src/main.tsx
@@ -12,24 +12,15 @@ import ITConsultingPage from './pages/ITConsultingPage.tsx';
 import CareersPage from './pages/CareersPage.tsx';
 import './index.css';
 
-const logoSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-  <defs>
-    <linearGradient id="g" x1="0" x2="1" y1="0" y2="1">
-      <stop offset="0" stop-color="#3BAFDA"/>
-      <stop offset="1" stop-color="#8C75FF"/>
-    </linearGradient>
-  </defs>
-  <path d="M34 6l-18 26h12l-4 20 22-28H36l6-18z" fill="url(#g)"/>
-</svg>`;
-const faviconDataUrl = `data:image/svg+xml;utf8,${encodeURIComponent(logoSvg)}`;
+const faviconUrl = new URL('./logo/bit4git logo.png', import.meta.url).href;
 let faviconLink = document.querySelector('link[rel="icon"]') as HTMLLinkElement | null;
 if (!faviconLink) {
   faviconLink = document.createElement('link');
   faviconLink.rel = 'icon';
   document.head.appendChild(faviconLink);
 }
-faviconLink.type = 'image/svg+xml';
-faviconLink.href = faviconDataUrl;
+faviconLink.type = 'image/png';
+faviconLink.href = faviconUrl;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
